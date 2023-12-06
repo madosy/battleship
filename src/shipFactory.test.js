5d1df0cedc350b2e5cfd5bc122e8c34ac433f5bc
Dasom Heo <dasomee@outlook.com>
2023-12-06 14:17:49 -0500

    Add files for webpack

--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -1,7 +1,5 @@
 import { shipFactory } from "./shipFactory";
 
-test.skip("Ships should have a hit() function that increases the number of ‘hits’ in your ship.", () => {});
-
 test("isSunk() should be a function that calculates it based on their length and the number of ‘hits’.", () => {
   const newShip = shipFactory(4);
   expect(newShip.isSunk()).toBe(false);
